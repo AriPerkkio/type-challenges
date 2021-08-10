59959df7932c7d7e9e90d4d32bc382e18816c3bb
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-10 17:52:25 +0000

    Formatting to ignore tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-    root: true,
-    env: {
-        es6: true,
-        node: true,
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    plugins: ['prettier'],
-    extends: ['@antfu/eslint-config-ts', 'plugin:prettier/recommended'],
-    rules: {
-        'prettier/prettier': 'error',
-        'import/first': 'off',
-        'no-unused-vars': 'off',
-        'no-use-before-define': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-    },
-};
+  root: true,
+  env: {
+    es6: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['prettier'],
+  extends: ['@antfu/eslint-config-ts', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/first': 'off',
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
+  }
+}
