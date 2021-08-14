3541c3e728ed5da0dbc25206606c10ccf3458435
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-14 15:04:20 +0000

    Q898

--- questions/898-easy-includes/template.ts
+++ questions/898-easy-includes/template.ts
@@ -1,1 +1,17 @@
-type Includes<T extends readonly any[], U> = any;
+// #1
+type SimpleIncludes<
+  List extends readonly any[],
+  Item
+> = Item extends List[number] ? true : false
+
+type TypeEqualsHack<First extends string, Second> = Second extends First
+  ? { [_ in First]: true }[Second] extends true
+    ? true
+    : false
+  : false
+
+// #2
+type Includes<List extends readonly any[], Item> = TypeEqualsHack<
+  List[number], // Doesn't really meet `extends string` requirement but seems to pass
+  Item
+>
