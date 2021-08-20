de240ff6312e8a6c178cf74c6963c9b5f97c9f2a
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-20 13:34:56 +0000

    Q191

--- questions/191-medium-append-argument/template.ts
+++ questions/191-medium-append-argument/template.ts
@@ -1,1 +1,5 @@
-type AppendArgument<Fn, A> = any
+type AppendArgument<Fn extends Function, A> = Fn extends (
+  ...args: infer Args
+) => any
+  ? (...args: [...Args, A]) => ReturnType<Fn>
+  : never
