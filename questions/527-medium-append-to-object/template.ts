6f2dcae7d9953d329fa2480612a2e5c726e5a81a
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-20 13:54:12 +0000

    Q527

--- questions/527-medium-append-to-object/template.ts
+++ questions/527-medium-append-to-object/template.ts
@@ -1,1 +1,9 @@
-type AppendToObject<T, U, V> = any
+type AppendToObject<
+  Type extends {},
+  Key extends string,
+  Value,
+  InputAsObject = { [_ in Key]: Value },
+  Union = Type & InputAsObject
+> = {
+  [CombinedKeys in keyof Union]: Union[CombinedKeys]
+}
