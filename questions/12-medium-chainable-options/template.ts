4127361c86d46009a7cef71174c68e64c2baefd7
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-22 08:19:51 +0000

    Q12

--- questions/12-medium-chainable-options/template.ts
+++ questions/12-medium-chainable-options/template.ts
@@ -1,4 +1,17 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type ChainableOption = { [key: string]: any }
+
+type MergeChainableOptions<
+  A,
+  B,
+  Union extends A & B = A & B,
+  Keys extends keyof Union = keyof Union
+> = { [Key in Keys]: Union[Key] }
+
+type Chainable<T extends ChainableOption = {}> = {
+  option<Key extends string, Value extends any>(
+    key: Key,
+    value: Value
+  ): Chainable<MergeChainableOptions<T, { [_ in Key]: Value }>>
+
+  get(): T
 }
