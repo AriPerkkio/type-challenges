53c056c740dcb4374355d092c3210cfc6babf62b
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-22 07:42:46 +0000

    Q110

--- questions/110-medium-capitalize/template.ts
+++ questions/110-medium-capitalize/template.ts
@@ -1,1 +1,8 @@
-type Capitalize<S extends string> = any
+type CharacterToUppercase = { /* a-z */ f: 'F' }
+
+type CapitalizeMedium<Text extends string> =
+  Text extends `${infer FirstCharacter}${infer Rest}`
+    ? FirstCharacter extends keyof CharacterToUppercase
+      ? `${CharacterToUppercase[FirstCharacter]}${Rest}`
+      : Text
+    : ''
