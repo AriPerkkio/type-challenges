568529fe02fcbbd6b9ce2abc3d55c114099aad41
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-26 17:34:41 +0000

    Q9

--- questions/9-medium-deep-readonly/template.ts
+++ questions/9-medium-deep-readonly/template.ts
@@ -1,1 +1,7 @@
-type DeepReadonly<T> = any
+type ExtendsObject<T> = T extends Function ? false : T extends {} ? true : false
+
+type DeepReadonly<T extends { [_: string]: any }> = {
+  readonly [Key in keyof T]: ExtendsObject<T[Key]> extends true
+    ? DeepReadonly<T[Key]>
+    : T[Key]
+}
