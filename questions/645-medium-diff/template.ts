5c2ec521bb807b8d1ca010453b0d0ba2b6fec827
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-30 16:50:29 +0000

    Q645

--- questions/645-medium-diff/template.ts
+++ questions/645-medium-diff/template.ts
@@ -1,1 +1,11 @@
-type Diff<O, O1> = any
+type Diff<First, Second> = {
+  [Key in
+    | Exclude<keyof First, keyof Second>
+    | Exclude<keyof Second, keyof First>]: Key extends keyof First
+    ? Key extends keyof Second
+      ? never
+      : First[Key]
+    : Key extends keyof Second
+    ? Second[Key]
+    : never
+}
