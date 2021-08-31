e8c8f7b03f1ea3dd8ce33aba4a35f23f57476841
Ari Perkkiö <ari.perkkio@gmail.com>
2021-08-31 17:34:57 +0000

    Q2070

--- questions/2070-medium-drop-char/template.ts
+++ questions/2070-medium-drop-char/template.ts
@@ -1,1 +1,6 @@
-type DropChar<S, C> = any
+type DropChar<
+  Text extends string,
+  Character extends string
+> = Text extends `${infer Prefix}${Character}${infer Postfix}`
+  ? DropChar<`${Prefix}${Postfix}`, Character>
+  : Text
